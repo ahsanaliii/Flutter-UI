7a9b89975c69c31333b74de92cc4df8194636a4e
Ahsan Ali <mahsanalii062@gmail.com>
2025-06-11 17:02:47 +0500

    video controller completed with all custom functionalities

--- videoplayer/lib/video_controller.dart
+++ videoplayer/lib/video_controller.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:get/get.dart';
@@ -8,6 +9,20 @@ class VideoController extends GetxController {
   final videoFile = Rx<File?>(null);
   final videoPlayerController = Rx<VideoPlayerController?>(null);
   final isPlaying = false.obs;
+  final showController = true.obs;
+  Timer? hideTimer;
+
+  Future<void> startHideTimer() async {
+    hideTimer?.cancel();
+    hideTimer = Timer(Duration(seconds: 2), () {
+      showController.value = false;
+    });
+  }
+
+  Future<void> resetHideTimer() async {
+    showController.value = true;
+    startHideTimer();
+  }
 
   Future<void> pickVideo() async {
     final picker = ImagePicker();
@@ -26,7 +41,44 @@ class VideoController extends GetxController {
       await controller.initialize();
       controller.play();
       videoPlayerController.value = controller;
+      startHideTimer();
       update();
     }
+    resetHideTimer();
+  }
+
+  void togglePlayPause() {
+    final controller = videoPlayerController.value;
+    if (controller != null) {
+      if (controller.value.isPlaying) {
+        controller.pause();
+        showController.value = true;
+        isPlaying.value = true;
+      } else {
+        controller.play();
+        isPlaying.value = false;
+      }
+    }
+    resetHideTimer();
+  }
+
+  void seekForward() {
+    final controller = videoPlayerController.value;
+    if (controller != null && controller.value.isInitialized) {
+      final currentPosition = controller.value.position;
+      final target = currentPosition + Duration(seconds: 1);
+      controller.seekTo(target);
+    }
+    resetHideTimer();
+  }
+
+  void seekBackward() {
+    final controller = videoPlayerController.value;
+    if (controller != null && controller.value.isInitialized) {
+      final currentPosition = controller.value.position;
+      final target = currentPosition - Duration(seconds: 1);
+      controller.seekTo(target > Duration.zero ? target : Duration.zero);
+    }
+    resetHideTimer();
   }
 }
--- videoplayer/lib/video_page.dart
+++ videoplayer/lib/video_page.dart
@@ -17,7 +17,7 @@ class VideoPage extends StatelessWidget {
             child: Column(
               children: [
                 videoController.videoPlayerController.value != null
-                    ? Column(
+                    ? Stack(
                       children: [
                         AspectRatio(
                           aspectRatio:
@@ -26,10 +26,99 @@ class VideoPage extends StatelessWidget {
                                   .value!
                                   .value
                                   .aspectRatio,
-                          child: VideoPlayer(
-                            videoController.videoPlayerController.value!,
+                          child: GestureDetector(
+                            onTap: () {
+                              videoController.showController.value =
+                                  !videoController.showController.value;
+                            },
+                            onLongPress: () {
+                              videoController.videoPlayerController.value!
+                                  .setPlaybackSpeed(2.0);
+                              videoController.showController.value = true;
+                            },
+                            onLongPressUp: () {
+                              videoController.videoPlayerController.value!
+                                  .setPlaybackSpeed(1.0);
+                              videoController.showController.value = false;
+                            },
+                            child: VideoPlayer(
+                              videoController.videoPlayerController.value!,
+                            ),
                           ),
                         ),
+                        if (videoController.showController.value)
+                          Positioned(
+                            // top: 8,
+                            bottom: 50,
+                            left: 0,
+                            right: 0,
+                            child: SizedBox(
+                              height: 10,
+                              child: VideoProgressIndicator(
+                                colors: VideoProgressColors(
+                                  backgroundColor: Colors.grey,
+                                  playedColor: Colors.red,
+                                ),
+
+                                videoController.videoPlayerController.value!,
+                                allowScrubbing: true,
+                              ),
+                            ),
+                          ),
+
+                        if (videoController.showController.value)
+                          Positioned(
+                            bottom: 0,
+                            left: 40,
+                            right: 40,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                IconButton(
+                                  onPressed: videoController.seekBackward,
+                                  icon: Icon(
+                                    Icons.arrow_back,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: videoController.togglePlayPause,
+
+                                  icon: Icon(
+                                    videoController.isPlaying.value
+                                        ? Icons.play_arrow
+                                        : Icons.pause,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: videoController.seekForward,
+                                  icon: Icon(
+                                    Icons.arrow_forward,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        if (videoController.showController.value)
+                          Positioned(
+                            top: 20,
+                            bottom: 20,
+                            left: 10,
+                            right: 10,
+                            child: GestureDetector(
+                              onTap: videoController.togglePlayPause,
+
+                              child: Icon(
+                                videoController.isPlaying.value
+                                    ? Icons.play_arrow
+                                    : Icons.pause,
+                                color: Colors.white,
+                                size: 60,
+                              ),
+                            ),
+                          ),
                       ],
                     )
                     : Padding(
